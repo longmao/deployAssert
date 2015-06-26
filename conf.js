b70004f12631bea59cf2920faf4b48f1ebd94619
longmao <unclelongmao@gmail.com>
2015-06-26 14:33:01 +0800

    add default api hosty

--- conf.js
+++ conf.js
@@ -3,7 +3,7 @@ module.exports = function () {
 	var conf = {
 		APP_FOLDER : process.env.folder ||  "www5",
 		API_JS_FILE : (process.env.folder ||  "www5" ) + "/static/script/main.js",
-		API_HOST : process.env.api_host
+		API_HOST : process.env.api_host || "api.yeahmobi.com"
 	}
     return conf
 }
